b1f8dfdcf109168bfd0a0a3fa2d9413c35fd7ff8
Promgirl65 <olayinkabusolami@gmail.com>
2025-11-15 00:53:17 +0100

    recipe-sharing-app

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,10 +1,7 @@
 import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
-  recipes: [
-    { id: 1, title: 'Spaghetti', description: 'Delicious spaghetti with tomato sauce' },
-    { id: 2, title: 'Pancakes', description: 'Fluffy pancakes with syrup' }
-  ],
+  recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
 }));
