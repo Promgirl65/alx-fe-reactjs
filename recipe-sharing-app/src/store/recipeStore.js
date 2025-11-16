77d84541c21107dd960f285a697caed18076d60f
Promgirl65 <olayinkabusolami@gmail.com>
2025-11-16 17:41:50 +0100

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,26 +1,26 @@
-// src/store/recipeStore.js
 import { create } from 'zustand';
 
-const useRecipeStore = create((set) => ({
-  recipes: [
-    { id: 1, title: 'Spaghetti', description: 'Delicious spaghetti with tomato sauce' },
-    { id: 2, title: 'Pancakes', description: 'Fluffy pancakes with syrup' },
-  ],
+export const useRecipeStore = create((set) => ({
+  recipes: [],
 
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  // ADD a new recipe
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, { id: Date.now(), ...recipe }]
+    })),
 
-  updateRecipe: (updatedRecipe) =>
+  // UPDATE an existing recipe
+  updateRecipe: (id, updatedData) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
+        recipe.id === id ? { ...recipe, ...updatedData } : recipe
+      )
     })),
 
+  // DELETE a recipe
   deleteRecipe: (id) =>
     set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+      recipes: state.recipes.filter((recipe) => recipe.id !== id)
+    }))
 }));
-
 export default useRecipeStore;
